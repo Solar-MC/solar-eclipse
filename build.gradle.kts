ce600cdcdeb9575340088dd640ba1a257731f36b
hYdos <haydenv06@gmail.com>
2021-05-25 21:00:52 +1000

    publishing setup

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     java
     id("java-gradle-plugin")
+    id("maven-publish")
     id("com.gradle.plugin-publish") version "0.12.0"
 }
 
@@ -12,6 +13,7 @@ repositories {
     maven(url = "https://maven.quiltmc.org/")
     maven(url = "https://maven.fabricmc.net/")
     maven(url = "https://jitpack.io")
+    maven(url = "gcs://devan-maven")
     mavenCentral()
 }
 
@@ -44,8 +46,27 @@ dependencies {
 gradlePlugin {
     plugins {
         create("Eclipse") {
+            displayName = "Solar Eclipse"
             id = "com.solarmc.eclipse"
             implementationClass = "com.solarmc.eclipse.EclipsePlugin"
+        }
+    }
+}
+
+publishing {
+    repositories {
+        // Add repositories to publish to here.
+        if (project.hasProperty("maven_url")) {
+            maven {
+                val mavenUrl = project.property("maven_url") as String
+                url = uri(mavenUrl)
+                if (mavenUrl.startsWith("http") && project.hasProperty("maven_username") && project.hasProperty("maven_password")) {
+                    credentials {
+                        username = project.property("maven_username") as String
+                        password = project.property("maven_password") as String
+                    }
+                }
+            }
         }
     }
 }
